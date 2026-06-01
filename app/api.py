26292edab61b6ab298feff3467c3c645aa5e3884
chernova2601-pixel <chernova2601@gmail.com>
2026-06-01 14:20:33 +0300

    Add model_v1.pkl, update api.py, adjust .gitignore

--- app/api.py
+++ app/api.py
@@ -1,12 +1,10 @@
-import pickle
-import numpy as np
 import logging
-from pythonjsonlogger import jsonlogger
 from flask import Flask, request, jsonify
+from pythonjsonlogger import jsonlogger
+from model_handler import ModelHandler
 
 app = Flask(__name__)
 
-# Настройка логирования в JSON
 logHandler = logging.StreamHandler()
 formatter = jsonlogger.JsonFormatter()
 logHandler.setFormatter(formatter)
@@ -14,9 +12,7 @@ logger = logging.getLogger()
 logger.addHandler(logHandler)
 logger.setLevel(logging.INFO)
 
-# Загрузка модели
-with open('models/credit_default_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+model_handler = ModelHandler(model_path='models/model_v1.pkl')
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -29,10 +25,8 @@ def predict():
     if not data or 'features' not in data:
         logger.warning({"endpoint": "/predict", "error": "Missing features key"})
         return jsonify({'error': 'Missing "features" key'}), 400
-    features = np.array(data['features']).reshape(1, -1)
-    pred = model.predict(features)[0]
-    prob = model.predict_proba(features)[0][1]
-    response = {'prediction': int(pred), 'probability': float(prob)}
+    pred, prob = model_handler.predict(data['features'])
+    response = {'prediction': pred, 'probability': prob}
     logger.info({"endpoint": "/predict", "features": data['features'], "response": response})
     return jsonify(response)
 
